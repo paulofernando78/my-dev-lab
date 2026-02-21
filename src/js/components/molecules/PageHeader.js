c05a137fb0cce723d4d0e4aa8299aa5b7bbc73da
Paulo Silva <paulofernando78@gmail.com>
2026-02-21 05:00:41 -0300

    wip

--- src/js/components/molecules/PageHeader.js
+++ src/js/components/molecules/PageHeader.js
@@ -1,26 +1,24 @@
 import componentStyles from "@css/imports/component.css?inline";
 
 const style = /* css */ `
-  .ribbon {
+  .page-header {
     background-color: var(--category-bg-color);
     border-radius: 5px;
-    height: 60px;
+    color: var(--category-color);
+    width: 100%;
+    padding: 12px;
     position: relative;
-    width: 100%
+    margin-bottom: 16px
   }
   
   .icons {
     position: absolute;
     bottom: -15px;
-    left: 10px;
+    right: 20px;
     width: 40px;
     height: auto;
 
     filter: drop-shadow(0px 0px 1px black);
-  }
-  
-  .headings {
-    margin: 32px 0 20px 0;
   }
 
   h1:has(+ h2) {
@@ -53,58 +51,57 @@ class PageHeader extends HTMLElement {
   }
 
   render() {
-    const categoryAttr = this.getAttribute("category")
-    const unitAttr = this.getAttribute("unit")
-    const pageAttr = this.getAttribute("page")
-
     const categoryStyles = {
       HTML: {
         background: "var(--orange-6)",
-        icon: "/assets/images/icons/html5.svg"
+        color: "#fff",
+        icon: "/assets/images/icons/html5.svg",
       },
       CSS: {
         background: "var(--purple-6)",
-        icon: "/assets/images/icons/css.svg"
+        color: "#fff",
+        icon: "/assets/images/icons/css.svg",
       },
       Javascript: {
-        background: "var(--yellow-3)",
-        icon: "/assets/images/icons/javascript.svg"
+        background: "#F0DB4F",
+        icon: "/assets/images/icons/javascript.svg",
       },
       "Node JS": {
         background: "var(--green-7)",
-        icon: "/assets/images/icons/nodejs.svg"
-      }
-    }
+        color: "#fff",
+        icon: "/assets/images/icons/nodejs.svg",
+      },
+    };
+
+    const categoryAttr = this.getAttribute("category");
+    const unitAttr = this.getAttribute("unit");
+    const pageAttr = this.getAttribute("page");
 
     const config = categoryStyles[categoryAttr] ?? {
       background: "var(--slate-2)",
-      icon: ""
-    }
+    };
 
     this.style.setProperty("--category-bg-color", config.background);
+    this.style.setProperty("--category-color", config.color ?? "#18191F");
 
     this.shadowRoot.innerHTML = /* HTML */ `
       <style>
         ${componentStyles}
         ${style}
       </style>
-      <div class="ribbon">
-      ${config.icon
-        ? /* html */`
-          <img src="${config.icon}" class="icons"/>
-        `
-        : ""
-      }
+      <div class="page-header">
+        <div>
+          <h1><b>${categoryAttr}</b></h1>
+          ${pageAttr ? /* html */ `<h2><b>${pageAttr}</b></h2>` : ""}
+          ${unitAttr ? /* html */ `<h3><b>${unitAttr}</b></h3>` : ""}
+        </div>
+        ${config.icon
+          ? /* html */ `<img src="${config.icon}" class="icons"/>`
+          : ""}
       </div>
-      <div class="headings">
-        <h1><b>${categoryAttr}</b></h1>
-        ${pageAttr ? /* html */`<h2><b>${pageAttr}</b></h2>` : ""}
-        ${unitAttr ? /* html */`<h3><b>${unitAttr}</b></h3>` : ""}
-      </div>
-      <hr />
-      `;
-    }
+    `;
   }
+}
 
 customElements.define("wc-page-header", PageHeader);
 export default PageHeader;
